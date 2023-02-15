0b03dc8b65d87932727bd023dee9f51e8cdbafe5
Clerseri <125460813+Clerseri@users.noreply.github.com>
2023-02-15 22:55:00 +1100

    Update TrackList.js

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -6,7 +6,7 @@ class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        {this.props.tracks[0].name} 
+        {this.props.tracks.length > 0 && this.props.tracks[0].name} 
         {/* <button className="Playlist-save">SAVE TO SPOTIFY</button>
         {this.props.tracks.map((track) => {
           return <Track track={track} key={track.id} />;
